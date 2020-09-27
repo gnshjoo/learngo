e3f810bfc6846dfe853a0e6129df44eafd1cf5d3
david Joo <gnshjoo@gmail.com>
2020-09-27 18:04:23 +0900

    recive String

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
 
 func main() {
-	totalLenght, _ := lenAndUpper("nico")
-	fmt.Println(totalLenght)
+	repeatMe("nico", "lynn", "dal", "marl", "flynn")
 }
