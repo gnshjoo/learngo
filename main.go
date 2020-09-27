c8529c64e5118cfbdffff963fe409444ece5ee1d
david Joo <gnshjoo@gmail.com>
2020-09-27 18:50:59 +0900

    how to use defer

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 )
 
 func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
 	length = len(name)
 	uppercase = strings.ToUpper(name)
 	return
 }
 
 func main() {
-	totalLenght, _ := lenAndUpper("nico")
-	fmt.Println(totalLenght)
+	totalLenght, upper := lenAndUpper("nico")
+	fmt.Println(totalLenght, upper)
 }
