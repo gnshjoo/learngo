13e91e98a290f0c1e57c04171808f0d694d3fd94
david Joo <gnshjoo@gmail.com>
2020-09-28 14:30:13 +0900

    golang -> Maps

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	names := []string{"nico", "lynn", "dal"}
-	names = append(names, "flynn")
-	fmt.Println(names)
+	nico := map[string]string{"name": "nico", "age": "12"}
+	fmt.Println(nico)
+
+	for key, value := range nico {
+		fmt.Println(key, value)
+	}
+
 }
