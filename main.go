0e6cc77e45492848f516b16fea2accdeec6de264
david Joo <gnshjoo@gmail.com>
2020-09-28 09:42:37 +0900

    different way use for

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ func superAdd(numbers ...int) int {
 	for index, number := range numbers {
 		fmt.Println(index, number)
 	}
+
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println(numbers[i])
+	}
 	return 1
 }
 
